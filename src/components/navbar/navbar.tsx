2b80a21a944e8e92282cbf1ab90cf04631481564
Elin Nyman <elin.nyman87@gmail.com>
2025-02-09 18:53:34 +0100

    fix: Change to using grid instead of flex on navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -28,33 +28,24 @@ const Navbar = () => {
 						style={{ objectFit: "cover", width: "100%", alignItems: "center" }}
 					/>
 				</div>
-				{hamburgerMenu()}
 			</div>
 			<div className="nav-right">
-				<div className="nav-link">Home</div>
-				<div className="nav-link">Menu</div>
-				<div className="nav-link">Blog</div>
-				<div className="nav-link">Contact</div>
-				<OrderNowButton />
+				<div className="hamburger-container">
+					<IconButton
+						icon={isMenuOpen ? "close" : "hamburger"}
+						onClick={() => setMenuOpen(!isMenuOpen)}
+					/>
+				</div>
 			</div>
-			<div className="nav-links-container">
-				{isMenuOpen && (
-					<ul className="nav-links">
-						<li>
-							<a href="#home">Home</a>
-						</li>
-						<li>
-							<a href="#about">About</a>
-						</li>
-						<li>
-							<a href="#services">Services</a>
-						</li>
-						<li>
-							<a href="#contact">Contact</a>
-						</li>
-					</ul>
-				)}
-			</div>
+			{isMenuOpen && (
+				<div className={`nav-links-container ${isMenuOpen ? "open" : ""}`}>
+					<div className="nav-link">Home</div>
+					<div className="nav-link">Menu</div>
+					<div className="nav-link">Blog</div>
+					<div className="nav-link">Contact</div>
+					<OrderNowButton />
+				</div>
+			)}
 		</nav>
 	);
 };
